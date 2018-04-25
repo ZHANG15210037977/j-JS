50084b388a3ebfd6e9706b3f093ced5ef53cd10c
Amu <773961013@qq.com>
2018-04-25 23:28:30 +0800

    踩了定时器和函数赋值的坑

--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,9 @@
 let register = (function () {
     let register_code = null;
     let z_index = 2000;
+    let max = 60;
+
+
     //提示信息对象数组
     let messageList = [{
         message: '请输入正确的手机号码',
@@ -31,6 +34,14 @@ let register = (function () {
         message: 'xxx',
         title: '×',
         bg_color: '#FF4949'
+    },{
+        message: '请输入六位验证码',
+        title: '×',
+        bg_color: '#FF4949'
+    },{
+        message: '输入验证码有误',
+        title: '×',
+        bg_color: '#FF4949'
     }]
 
     function addNode(messageList) {//表单消息提示处理函数，参数为提示信息对象
@@ -56,41 +67,58 @@ let register = (function () {
 
     function check_data(inputeListe) {//表单数据监测函数，参数为input标签的 NodeList对象
 
-        for (let i = 0; i < 3; i++) {
-            if (inputeListe[i].value === '') {
-
-                addNode(messageList[i]);
-                return false;
-            }
-        }
-
-        if ((inputeListe[0].value + '').length < 11 && inputeListe[0].value !== '') {
-
+        //手机号判断
+        if ((inputeListe[0].value + '').length < 11 || inputeListe[0].value === '') {
             addNode(messageList[0]);
             return false;
-
         }
 
-
-        if (inputeListe[1].value.length < 6 && inputeListe[1].value !== '') {
-
+        //密码判断
+        if (inputeListe[1].value.length < 6 || inputeListe[1].value === '') {
             addNode(messageList[1]);
             return false;
-
         }
 
-        if (inputeListe[1].value !== inputeListe[2].value && inputeListe[1].value !== '' && inputeListe[2].value !== '') {
+        //判断是否确认密码
+        if (inputeListe[2].value === '') {
+            addNode(messageList[2]);
+            return false;
+        }
 
+        //判断密码输入是否一致判断
+        if (inputeListe[1].value !== inputeListe[2].value && inputeListe[1].value !== '' && inputeListe[2].value !== '') {
             addNode(messageList[4]);
             return false;
-
         }
+
         return true;
 
     }
 
 
-    function get_code() {
+    function insertTimeout(event) {//60s计时函数
+        
+        if (max !== 0) {
+            event.target.innerText = `${max}s后重新获取`;
+            max--;
+        } else {
+            event.target.onclick = register.get_code;
+            event.target.onclick.arguments = event;
+            event.target.innerText = '获取验证码';
+            event.target.style.backgroundColor = '#46b036';
+            max = 60;
+            return;
+        }
+
+        setTimeout(()=>{
+
+            insertTimeout(event);
+
+        }, 1000);
+
+    }
+
+    function get_code(event) {//获取验证码函数
         let inputeListe = document.getElementsByTagName('input');
 
 
@@ -105,15 +133,22 @@ let register = (function () {
                         if (res.status == 1) {
                             addNode(messageList[3]);
                             register_code = res.data.code;
+                            //这部分逻辑不完整  因为后台接口不全 
+                            event.target.onclick = null;
+                            event.target.innerText = `60秒后重新获取`;
+                            event.target.style.backgroundColor = '#999999';
+                            setTimeout(insertTimeout(event),1000);
+
                         } else {
                             messageList[5].message = res.info;
                             addNode(messageList[5]);
+                            register_code = '123456';//后台默认返回‘123456’
                         }
                     } else {
                         messageList[5].message = '请求出错，返回状态码为:' + xhr.status
                         addNode(messageList[5]);
                     }
-                    
+
                 }
 
             }
@@ -121,8 +156,10 @@ let register = (function () {
             xhr.open("post", "http://yjhapi.agxx.club/iweb/Sendsms/send", true);
             xhr.setRequestHeader("Cotent-Type", "application/json");
             let para = JSON.stringify({
-                mobile: inputeListe[0].value
+                mobile:inputeListe[0].value
             });
+
+            console.log(para);
             xhr.send(para);
 
         }
@@ -133,8 +170,30 @@ let register = (function () {
 
     }
 
+    function register_user(){
+
+        let inputeListe = document.getElementsByTagName('input');
+        console.log(inputeListe);
+
+        let key1 = check_data(inputeListe);
+        let key2 = true;
+        
+        if(inputeListe[3].value == ''){
+            addNode(messageList[6]);
+            key2 = false;
+        }
+
+        if(inputeListe[3].value != '' && inputeListe[3].value != register_code){
+            addNode(messageList[7]);
+            key2 = false;
+        }
+
+        console.log(key1);
+        console.log(key2);
+    }
     return {
-        get_code
+        get_code,
+        register_user
     }
 })()
 
